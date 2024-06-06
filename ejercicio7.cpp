0e1bdd4848fbda56e4f6af381ab19330bf1fcbf9
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-06 11:16:00 -0600

    Add files via upload

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
-    int ar, bs, l, al, bsm, k;
-    cout << "De que figura quiere sacar el area:\n";
-    cout << "1.-Triangulo 2.-Trapecio 3.-Rectangulo\n";
-    cin >> k;
-    
-    if (k == 1) {
-        cout << "Ingresa la base de tu triangulo: ";
-        cin >> bs;
-        cout << "Ingresa la altura del triangulo: ";
-        cin >> al;
-        ar = (bs * al) / 2;
-        cout << "El area de tu triangulo es: " << ar << endl;
-    }
-    else if (k == 2) {
-        cout << "Ingresa la base mayor de tu trapecio: ";
-        cin >> bsm;
-        cout << "Ingresa la base menor del trapecio: ";
-        cin >> bs;
-        cout << "Ingresa la altura del trapecio: ";
-        cin >> al;
-        ar = ((bsm + bs) * al) / 2;  
-        cout << "El area de tu trapecio es: " << ar << endl;
-    }
-    else if (k == 3) {
-        cout << "Ingresa la base de tu rectangulo: ";
-        cin >> bs;
-        cout << "Ingresa la altura del rectangulo: ";
-        cin >> al;
-        ar = bs * al;
-        cout << "El area de tu rectangulo es: " << ar << endl;
+int main() {
+    int a, b, l, al, bm, f;
+
+    cout << "De que figura quieres sacar el area?\n";
+    cout << "1.- Calcular el area de un triangulo\n";
+    cout << "2.- Calcular el area de un trapecio\n";
+    cout << "3.- Calcular el area de un rectangulo\n";
+    cin >> f;
+
+    switch (f) {
+        case 1:
+            cout << "Ingresa la base de tu triangulo: ";
+            cin >> b;
+            cout << "Ingresa la altura del triangulo: ";
+            cin >> al;
+            a = (b * al) / 2;
+            cout << "El area de tu triangulo es de: " << a;
+            break;
+        case 2:
+            cout << "Ingresa la base mayor de tu trapecio: ";
+            cin >> bm;
+            cout << "Ingresa la base menor del trapecio: ";
+            cin >> b;
+            cout << "Ingresa la altura de tu trapecio: ";
+            cin >> al;
+            a = ((bm + b) * al) / 2;
+            cout << "El area de tu trapecio es de: " << a;
+            break;
+        case 3:
+            cout << "Ingresa la base de tu rectangulo: ";
+            cin >> b;
+            cout << "Ingresa la altura del rectangulo: ";
+            cin >> al;
+            a = b * al;
+            cout << "El area de tu rectangulo es de: " << a;
+            break;
+        default:
+            cout << "Opcion no valida";
+            break;
     }
 
     return 0;
 }
+
