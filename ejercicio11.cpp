8ae16ef480776d2cc1c0ae9cd4c9ecdec737498d
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-06 11:36:38 -0600

    Add files via upload

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int num, sum = 0, cont = 0;
     
     do {
-        cout << "Introduce un numero 0 para finalizar: ";
+        cout << "Introduce un numero (0 para finalizar): ";
         cin >> num;
         
         if(num != 0) {
-            cout << "Has introducido el numero: " << num;
+            cout << "Has introducido el numero: " << num << "\n";
             sum += num;
             cont++;
         }
     } while(num != 0);
     
-    cout << "Se ha introducido el numero 0";
-    cout << "La cantidad de numeros es de: " << cont;
-    cout << "La suma de los numeros es: " << sum;
+    cout << "Se ha introducido el numero 0" << "\n";
+    cout << "La cantidad de numeros es de: " << cont << "\n";
+    cout << "La suma de los numeros es: " << sum << "\n";
     
     return 0;
 }
