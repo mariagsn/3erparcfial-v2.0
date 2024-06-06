6e2aeef4a0a63c5e4d4884abb215b51662d0a29e
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-06 12:06:04 -0600

    Add files via upload

--- ejercicio12.cpp
+++ ejercicio12.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 int main() {
+    int num1, num2, total;
 
-int num1,num2,total;
+    cout << "Dime el primer numero: ";
+    cin >> num1;
+    cout << "Dime el segundo numero: ";
+    cin >> num2;
 
-cout<<"dime el primer numero";
-cin>>num1;
-cout<<"dime el segundo numero";
-cin>>num2;
-total= num1%num2;
-cout<<"el total de la division es "<<total<<"\n";
-if(total-0){;
-cout<<" el numero no es divisible";
-}
-else{
-cout<<"el numero es divisible";
+    if (num2 == 0) {
+        cout << "No es posible dividir por cero." << "\n";
+        return 1; 
+    }
 
+    total = num1 % num2;
 
-}
-return 0;
+    cout << "El total de la division es " << total << "\n";
+
+    if (total != 0) {
+        cout << "El numero no es divisible" << "\n";
+    } else {
+        cout << "El numero es divisible" << "\n";
+    }
+
+    return 0;
 }
