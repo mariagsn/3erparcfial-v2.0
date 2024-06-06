ffb83064954c2d540e1ab07ca515a5b08d3c005c
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-06 12:57:25 -0600

    Add files via upload

---
+++ ejercicio14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1, num2;
+
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    if (num1 > num2) {
+        cout << num1 << " es mayor que " << num2 << "\n";
+            sum += num;
+    } else if (num1 < num2) {
+        cout << num1 << " es menor que " << num2 << "\n";
+            sum += num;
+    } else {
+        cout << "Ambos números son iguales" << "\n";
+            sum += num;
+    }
+
+    return 0;
+}
