58bda675c723067ec9a024db59d96ed4521890c8
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-06 21:14:13 -0600

    Add files via upload

---
+++ ejercicio27.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char nom[20];
+    char cont[20];
+    int intentos = 3;
+
+    do {
+        cout << "Introduce tu nombre: ";
+        cin >> nom;
+
+        cout << "Introduce tu contraseña: ";
+        cin >> cont;
+
+        if (strcmp(nom, "root") == 0 && strcmp(cont, "1234") == 0) {
+            cout << "Bienvenido al sistema." << "\n";
+            break;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                cout << "Nombre de usuario o contraseña incorrectos. Te quedan " << intentos << " intentos." << "\n";
+            } else {
+                cout << "Se ha superado el número de intentos permitido." << "\n";
+                break;
+            }
+        }
+    } while (intentos > 0);
+
+    return 0;
+}
