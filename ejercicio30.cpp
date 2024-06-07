3443f470e63bb28bf88412650caca87e6786e645
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-07 09:24:08 -0600

    Add files via upload

--- ejercicio30.cpp
+++ ejercicio30.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main() {
-    char nom[20];
-    char cont[20];
     int pro, cant;
-    int intentos = 3;
-    int importe = 0;
-
-    do {
-        cout << "Introduce tu nombre: ";
-        cin >> nom;
-
-        cout << "Introduce tu contraseña: ";
-        cin >> cont;
-
-        if (strcmp(nom, "maria") == 0 && strcmp(cont, "2b38") == 0) {
-            cout << "Bienvenido al sistema.\n";
-            break;
-        } else {
-            intentos--;
-            if (intentos > 0) {
-                cout << "Nombre de usuario o contraseña incorrectos. Te quedan " << intentos << " intentos.\n";
-            } else {
-                cout << "Se ha superado el número de intentos permitido.\n";
-                break;
-            }
-        }
-    } while (intentos > 0);
+    double importe = 0.0;
 
     cout << "Para terminar, introduce la cantidad de 0.\n\n";
     cout << "Muy bien, ingresa la cantidad de tus artículos vendidos: ";
@@ -40,7 +15,7 @@ int main() {
         if (pro < 0 || cant < 0) {
             cout << "Lo siento, las cantidades no pueden ser negativas. Ingresa otra cantidad.\n\n";
         } else {
-            float imp = pro * cant;
+            double imp = pro * cant;
             importe += imp;
         }
         cout << "\nCantidad: ";
@@ -51,3 +26,4 @@ int main() {
 
     return 0;
 }
+
