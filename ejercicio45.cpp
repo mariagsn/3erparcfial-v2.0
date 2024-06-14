9257b4c25f758009b80219e642469c0335a512f2
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-13 20:34:03 -0600

    Add files via upload

--- ejercicio45.cpp
+++ ejercicio45.cpp
@@ -1,47 +1,17 @@
 #include <iostream>
-#include <vector>
 #include <string>
-
 using namespace std;
 
 int main() {
-    // Definición de arreglos unidimensionales
-    string arreglo1[3];
-    int arreglo2[3];
-
-    arreglo1[0] = "Fernando";
-    arreglo1[1] = "Oscar";
-
-    arreglo2[0] = 74;
-    arreglo2[1] = 56;
-    arreglo2[2] = 10;
-
-    int matriz1[2][2] = {{1, 2}, {3, 4}};
-    string matriz2[2][2] = {{"A", "B"}, {"C", "D"}};
-
+    string arreglo1[2] = {"fernando", "oscar"};
+    int arreglo2[3] = {74, 56, 10};
     cout << "Arreglo 1:" << endl;
-    for (int i = 0; i < 3; ++i) {
-        cout << "El dato de la posicion " << i << " es: " << arreglo1[i] << endl;
-    }
-
-    cout << "Arreglo 2:" << endl;
-    for (int i = 0; i < 3; ++i) {
-        cout << "El dato de la posicion " << i << " es: " << arreglo2[i] << endl;
-    }
-
-    cout << "Matriz 1:" << endl;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            cout << "El dato en la posicion (" << i << ", " << j << ") es: " << matriz1[i][j] << endl;
-        }
-    }
-
-    cout << "Matriz 2:" << endl;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            cout << "El dato en la posicion (" << i << ", " << j << ") es: " << matriz2[i][j] << endl;
-        }
-    }
+    cout << "Dato en la posicion 0: " << arreglo1[0] << endl;
+    cout << "Dato en la posicion 1: " << arreglo1[1] << endl;
+    cout << "\nArreglo 2:" << endl;
+    cout << "Dato en la posicion 0: " << arreglo2[0] << endl;
+    cout << "Dato en la posicion 1: " << arreglo2[1] << endl;
+    cout << "Dato en la posicion 2: " << arreglo2[2] << endl;
 
     return 0;
 }
